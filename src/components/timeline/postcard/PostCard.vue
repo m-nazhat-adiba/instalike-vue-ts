db2a3e4f6d6c23af9422d3f055da70a7450951f1
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-11-08 15:29:11 +0700

    edited syntax props

--- src/components/timeline/postcard/PostCard.vue
+++ src/components/timeline/postcard/PostCard.vue
@@ -2,16 +2,17 @@
 import { Icon } from '@iconify/vue'
 import { ref } from 'vue'
 
-const isLiked = ref(false)
+interface Props {
+  title: string
+  url: string
+}
+
+const isLiked = ref<boolean>(false)
+const props = defineProps<Props>()
 
 const handleLike = () => {
   isLiked.value = !isLiked.value
 }
-
-defineProps({
-  title: String,
-  url: String
-})
 </script>
 
 <template>
@@ -20,7 +21,7 @@ defineProps({
       <img class="rounded-full h-6 w-6" src="../../../assets/logo.svg" />
       <h1 class="font-bold">Username</h1>
     </div>
-    <img :src="url" class="w-full h-auto" />
+    <img :src="props.url" class="w-full h-auto" />
     <div class="flex gap-2 px-4 text-xl">
       <div v-if="!isLiked" @click="handleLike">
         <Icon icon="mdi:heart-outline" />
@@ -32,7 +33,7 @@ defineProps({
       <Icon icon="iconamoon:send-light" />
     </div>
     <p class="text-xs px-5">
-      {{ title }}
+      {{ props.title }}
     </p>
   </div>
 </template>
