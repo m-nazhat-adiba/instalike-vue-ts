cd586cae1c7feb94a2b58487e13904261b101ba1
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-11-08 16:44:32 +0700

    added function to toggle between follow and followed status

--- src/components/search/UserCard.vue
+++ src/components/search/UserCard.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { defineProps, ref } from 'vue'
 
 interface User {
   name: string
   username: string
   image: string
+}
+
+const isFollowed = ref<boolean>(false)
+
+const handleFollow = () => {
+  isFollowed.value = !isFollowed.value
 }
 
 const { name, username, image } = defineProps<User>()
@@ -17,6 +23,15 @@ const { name, username, image } = defineProps<User>()
       <h1 class="font-semibold">{{ name }}</h1>
       <p class="text-sm">{{ username }}</p>
     </div>
-    <button class="px-3 py-1 bg-blue-500 rounded-full text-white">Follow</button>
+    <button
+      @click="handleFollow"
+      :class="{
+        'px-3 py-1 bg-blue-500 rounded-full text-white': !isFollowed,
+        'px-3 py-1 bg-white rounded-full text-black': isFollowed
+      }"
+    >
+      <p v-if="!isFollowed">Follow</p>
+      <p v-if="isFollowed">Followed</p>
+    </button>
   </div>
 </template>
