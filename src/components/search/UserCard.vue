123bd0f4b510df2e495063a5a98abc8726391e29
m-nazhat-adiba <140298565+m-nazhat-adiba@users.noreply.github.com>
2023-11-08 15:29:48 +0700

    first commit

---
+++ src/components/search/UserCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface User {
+  name: string
+  username: string
+  image: string
+}
+
+const { name, username, image } = defineProps<User>()
+</script>
+
+<template>
+  <div class="flex w-full items-center">
+    <img :src="image" class="h-12 w-12 rounded-full bg-gray-300" />
+    <div class="flex flex-col flex-grow ml-3">
+      <h1 class="font-semibold">{{ name }}</h1>
+      <p class="text-sm">{{ username }}</p>
+    </div>
+    <button class="px-3 py-1 bg-blue-500 rounded-full text-white">Follow</button>
+  </div>
+</template>
---
+++ src/views/Search/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import axios from 'axios'
+import Layout from '../../components/common/layout/Layout.vue'
+import UserCard from '../../components/search/UserCard.vue'
+import { onMounted, ref } from 'vue'
+
+interface User {
+  name: string
+  username: string
+  image: string
+}
+
+const userData = ref<Array<User> | null>(null)
+
+const fetchUserData = async () => {
+  try {
+    const response = axios.get('https://dummyjson.com/users')
+    userData.value = (await response).data.users.map((item) => ({
+      name: `${item.firstName} ${item.lastName}`,
+      username: item.username,
+      image: item.image
+    }))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => {
+  fetchUserData()
+})
+</script>
+
+<template>
+  <Layout>
+    <header class="px-5 font-bold mt-4 mb-5">
+      <h1>Discover People</h1>
+    </header>
+    <section class="flex flex-col px-5 gap-8">
+      <UserCard
+        v-for="item in userData"
+        :name="item.name"
+        :username="item.username"
+        :image="item.image"
+      />
+    </section>
+  </Layout>
+</template>
